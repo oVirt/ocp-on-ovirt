5be0fc50e2345b6af95d7505b77b4f0b4d691713
Evgeny Slutsky <eslutsky@redhat.com>
2020-07-12 14:28:38 +0300

    Added some comments for clearity
    Signed-off-by: Evgeny Slutsky <eslutsky@redhat.com>

--- ocp-on-rhv-ci/tools/ovirtci/cli/prFinder.go
+++ ocp-on-rhv-ci/tools/ovirtci/cli/prFinder.go
@@ -19,19 +19,24 @@ func main() {
 	for vmName, comment := range engineVms {
 		fields := strings.Split(vmName, "-")
 		log.Infof("Found vm %s with comment %s", vmName, comment)
+
+		//if a VM already have a comment then skip it
 		if comment != "" {
 			continue
 		}
 
+		//skip VMs that dont comply with our naming convention
 		if len(fields) > 2 {
 			cluster_id := fields[0]
 
-			cluster_id = "ovirt10"
+			//find the PR Link from the last received event for that cluster
 			prLink := oengine.GetLastPRForCluster(cluster_id)
 
 			if prLink != "" {
 				log.Infof("Updating vm %s - cluster - %s with PR %s", vmName, cluster_id, prLink)
 				oengine.AddComment(vmName, prLink)
+			} else {
+				log.Infof("Couldnt find PR for cluster %s", cluster_id)
 			}
 
 		}
