518db6a6587dd6c9a8ebfcd96f12d904d556557d
Gal-Zaidman <gzaidman@redhat.com>
2020-08-02 14:19:52 +0300

    update e2e runnner
    Signed-off-by: Gal-Zaidman <gzaidman@redhat.com>

--- ci-jobs/e2e-runner/e2e-runner.sh
+++ ci-jobs/e2e-runner/e2e-runner.sh
@@ -1,23 +1,77 @@
 #!/bin/bash
 
-help="Tool for running ovirt e2e jobs from the command line,\n
-you have to be connected to ovirt openshift ci namespace before running this tool\n
+help="
+Tool for generting ovirt e2e job template from the command line.
+
 
 Usage:
 e2e-runner OPTIONS
+
+Flags:
+-m --minimal            Create a minimal cluster and run a minimal test suite, this is used to run a regular PR job
+-c --csi                Create a minimal cluster and run the csi test suite, this is used to test the csi driver
+--no-lease              Don't acquire a boskos lease, this is very danguros since it can lead to overloading CI resources, use this flag only when testing new leases configuration.
+--no-template           CI job will create a tempalte, currently we are not providing all the configuration needed on install config and don't clean old template VM.
+--pause-after-setup     Pause for 3h after setup is successful, after you are done and want to continue log into the setup container and kill the sleep process
+--pause-before-setup    Pause for 3h before test is successful, after you are done and want to continue log into the test container and kill the sleep process
+--pause-after-test      Pause for 3h after test is successful, after you are done and want to continue log into the test container and kill the sleep process
+--tag
+--release
+--job-name
 "
 
-LEASE_TYPE="conformance"
+#4.4 || 4.5 || 4.6 || latest
 RELEASE_IMAGE_TAG="4.6"
-OPENSHIFT_INSTALL_RELEASE_IMAGE_OVERRIDE="registry.svc.ci.openshift.org/ocp/release:${RELEASE_IMAGE_TAG}"
+RELEASE_IMAGE_REPO="registry.svc.ci.openshift.org/ocp/release"
+OPENSHIFT_INSTALL_RELEASE_IMAGE_OVERRIDE="${RELEASE_IMAGE_REPO}:${RELEASE_IMAGE_TAG}"
+#conformance || minimal
+LEASE_TYPE="minimal"
+#conformance || minimal || csi
+TEST="minimal"
+JOB_NAME="e2e-job-${RELEASE_IMAGE_TAG}-${LEASE_TYPE}"
+#true || false
 USE_LEASE="true"
 USE_OVIRT_TEMPLATE="true"
 STOP_AFTER_SETUP="false"
-TEST="conformance"
 STOP_AFTER_TEST="false"
 STOP_BEFORE_TEST="true"
 
-echo $PWD
+# while [[ "$1" =~ ^- && ! "$1" == "--" ]]; do case $1 in
+#   -m | --minimal )
+#     LEASE_TYPE="minimal"
+#     TEST="minimal"
+#     ;;
+#   -c | --csi )
+#     LEASE_TYPE="minimal"
+#     TEST="csi"
+#     ;;
+#   --no-lease )
+#     USE_LEASE="false"
+#     ;;
+#   --no-template )
+#     USE_OVIRT_TEMPLATE="false"
+#     ;;
+#   --pause-after-setup )
+#     STOP_AFTER_SETUP="true"
+#     ;;
+#   --pause-before-test )
+#     STOP_BEFORE_TEST="true"
+#     ;;
+#   --pause-after-test )
+#     STOP_AFTER_TEST="true"
+#     ;;
+#   --tag )
+#     shift
+#     RELEASE_IMAGE_TAG="${1}"
+#   --release )
+#     shift
+#     RELEASE_IMAGE_REPO="${1}"
+#     ;;
+#   --job-name )
+#     shift
+#     JOB_NAME="${1}"
+#     ;;
+# esac; shift; done
 
 sed -e "s|PLACEHOLDER_LEASE_TYPE|\"${LEASE_TYPE}\"|" \
     -e "s|PLACEHOLDER_OPENSHIFT_INSTALL_RELEASE_IMAGE_OVERRIDE|\"${OPENSHIFT_INSTALL_RELEASE_IMAGE_OVERRIDE}\"|" \
@@ -26,4 +80,5 @@ sed -e "s|PLACEHOLDER_LEASE_TYPE|\"${LEASE_TYPE}\"|" \
     -e "s|PLACEHOLDER_STOP_AFTER_SETUP|\"${STOP_AFTER_SETUP}\"|" \
     -e "s|PLACEHOLDER_TEST|\"${TEST}\"|" \
     -e "s|PLACEHOLDER_STOP_AFTER_TEST|\"${STOP_AFTER_TEST}\"|" \
-    -e "s|PLACEHOLDER_STOP_BEFORE_TEST|\"${STOP_BEFORE_TEST}\"|" ./e2e-suite.in.yaml > e2e-job.yaml
+    -e "s|PLACEHOLDER_JOB_NAME|\"${JOB_NAME}\"|" \
+    -e "s|PLACEHOLDER_STOP_BEFORE_TEST|\"${STOP_BEFORE_TEST}\"|" /home/gzaidman/workspace/upstream/ocp/ocp-ci-tools/ci-jobs/e2e-runner/e2e-suite.in.yaml > ${JOB_NAME}.yaml
